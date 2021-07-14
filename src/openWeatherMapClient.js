ff80f23b388a01e05637bb4ecc7e25cc19dc35d1
till gilsbach <till.gilsbach@artcom.de>
2021-07-14 18:53:12 +0200

    Remove outdated content

--- src/openWeatherMapClient.js
+++ src/openWeatherMapClient.js
@@ -2,11 +2,8 @@ const fetch = require("node-fetch")
 
 const BASE_URL = "https://api.openweathermap.org/data/2.5"
 const API_CALL = "onecall"
-const LAT = "50.737"
-const LON = "7.098"
 const EXCLUDE_DATA = "hourly,minutely,alerts"
 const UNITS = "metric"
-const APP_ID = "063da9fd5923fe721ecfc32788ae8fcf"
 
 // For temperature in Fahrenheit and wind speed in miles/hour, use units=imperial
 // For temperature in Celsius and wind speed in meter/sec, use units=metric
