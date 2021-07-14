946ef25381cd4d02b56140432b0a168e24975e25
till gilsbach <till.gilsbach@artcom.de>
2021-07-14 17:37:14 +0200

    Add param for units

--- src/openWeatherMapClient.js
+++ src/openWeatherMapClient.js
@@ -5,10 +5,11 @@ const API_CALL = "onecall"
 const LAT = "50.737"
 const LON = "7.098"
 const EXCLUDE_DATA = "hourly,minutely,alerts"
-const APP_ID = ""
+const UNITS = "metric"
+const APP_ID = "063da9fd5923fe721ecfc32788ae8fcf"
 
 const getData = async () => {
-  const data = await fetch(`${BASE_URL}/${API_CALL}?lat=${LAT}&lon=${LON}&exclude=${EXCLUDE_DATA}&appid=${APP_ID}`)
+  const data = await fetch(`${BASE_URL}/${API_CALL}?lat=${LAT}&lon=${LON}&units=${UNITS}&exclude=${EXCLUDE_DATA}&appid=${APP_ID}`)
     .then(response => response.json())
   return data
 }
