96ca2713c8c35b8128d1049249a73c1de426b15f
till gilsbach <till.gilsbach@artcom.de>
2021-07-14 16:05:23 +0200

    Parameterize fetch request

--- src/openWeatherMapClient.js
+++ src/openWeatherMapClient.js
@@ -1,7 +1,14 @@
 const fetch = require("node-fetch")
 
-const getData = () => fetch(
-  "https://api.openweathermap.org/data/2.5/onecall?lat=50.737&lon=7.098&exclude=hourly,minutely,alerts&appid=063da9fd5923fe721ecfc32788ae8fcf"
-).then(response => response.json()).then(data => console.log(data))
+const BASE_URL = "https://api.openweathermap.org/data/2.5"
+const API_CALL = "onecall"
+const LAT = "50.737"
+const LON = "7.098"
+const EXCLUDE_DATA = "hourly,minutely,alerts"
+const APP_ID = "063da9fd5923fe721ecfc32788ae8fcf"
+
+const getData = () => fetch(`${BASE_URL}/${API_CALL}?lat=${LAT}&lon=${LON}&exclude=${EXCLUDE_DATA}&appid=${APP_ID}`)
+  .then(response => response.json())
+  .then(data => console.log(data))
 
 module.exports = getData
